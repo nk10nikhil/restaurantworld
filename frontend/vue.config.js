363aeec1f11f5d80c5069256b4f59638473daca9
nk10nikhil <nk10nikhil@gmail.com>
2025-04-30 00:09:20 +0530

    fix deploy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path');
+const fs = require('fs');
+const fsExtra = require('fs-extra');
 // module.exports = defineConfig({
 //   transpileDependencies: true
 // });
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../backend/restaurant_management'),
+  outputDir: path.resolve(__dirname, 'dist'),
   devServer: {
     host: 'localhost',
     port: 8080,
@@ -19,5 +21,21 @@ module.exports = {
         changeOrigin: true
       }
     }
+  },
+  configureWebpack: {
+    plugins: [
+      {
+        apply: (compiler) => {
+          compiler.hooks.afterEmit.tap('CopyToBackend', (compilation) => {
+            fsExtra.copySync(
+              path.resolve(__dirname, 'dist'),
+              path.resolve(__dirname, '../backend/restaurant_management'),
+              { overwrite: true }
+            );
+            console.log('Copied build files to backend/restaurant_management');
+          });
+        }
+      }
+    ]
   }
 }
